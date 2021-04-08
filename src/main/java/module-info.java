40026d121cc1ffc4c6b570f86db08fbda32820ba
schoddcl <schoddcl@miamioh.edu>
2021-04-08 15:34:42 -0400

    Added ant build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires com.microsoft.sqlserver.jdbc;
+   // requires com.microsoft.sqlserver.jdbc;
 	
 
     opens org.example to javafx.fxml;
