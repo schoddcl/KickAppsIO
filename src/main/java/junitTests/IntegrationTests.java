4f4ec377542301059c10a311eb3f7a138fee7391
sarrazlt <sarrazlt@miamioh.edu>
2021-05-04 14:35:57 -0400

    Adding Integration test

--- src/main/java/junitTests/IntegrationTests.java
+++ src/main/java/junitTests/IntegrationTests.java
@@ -3,6 +3,7 @@ package junitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,41 +14,23 @@ import org.example.SubmissionsController;
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 class IntegrationTests {
 
 	@Test
-	void CanUpdateSubmissionsTableFromAddWindow() {
-		AddController addController = new AddController();
-		assertTrue(addController.getSubmissionsForUser(3) instanceof ObservableList<?>);
+	void CanGetAProfessorByIDFromDataBase() {
+		DBConnector connector = new DBConnector();
+ 		Connection conn = connector.connect();
+ 		SubmissionsController submissionsController = new SubmissionsController();
+ 		ResultSet rs = submissionsController.getAllPendingSubmissions();
+ 		assertTrue(connector.getProfessorsObservableList(rs) instanceof ObservableList<?>);
 	}
 	
 	@Test
 	void CanUpdateSubmissionsTableFromAdminSubmissionsWindow() {
 		SubmissionsController submissionsController = new SubmissionsController();
 		assertTrue(submissionsController.setTable(3));
-	}
-	
-	@Test
-	void AddingProfessorShowsUpInDatabase() {
-		Professor testProf = new Professor(-1, "WhiteBoxTestFName", "WhiteBoxTestLName", 1.0, "WhiteBoxTest", "WhiteBoxTest", 1, "WhiteBoxTest");
-		DBConnector connector = new DBConnector();
- 		Connection conn = connector.connect();
-		assertTrue(connector.addProfessor(testProf));
-		ResultSet rs = connector.getProfessorResultSet(conn);
-		String firstName = "";
-		String lastName = "";
-		try {
-			while(rs.next()) {
-				firstName = rs.getString(3);
-				lastName = rs.getString(4);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		assertEquals("WhiteBoxTestFName", firstName);
-		assertEquals("WhiteBoxTestLName", lastName);
-		assertTrue(connector.deleteProfessorByFirstAndLastName(testProf));
 	}
 }
